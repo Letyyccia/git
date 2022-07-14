fc57a095a770f215950b94181aa9d1068fd6f7ea
Letyyccia <102097499+Letyyccia@users.noreply.github.com>
2022-07-14 15:59:45 -0300

    Create Aula01.java

---
+++ Aula01.java
@@ -0,0 +1,25 @@
+public class aula01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String nome = "leticia";
+        int idade = 17;
+        String cpf = "0927985789";
+
+        System.out.println ("meu nome é" + nome);
+        System.out.println ("Tenho " + idade + " anos");
+        System.out.println ("Meu CPF é" + cpf);
+
+        nome = sc.next();
+        idade = sc.nextInt();
+        cpf = sc.next();
+
+        System.out.println ("meu nome é" + nome);
+        System.out.println ("Tenho " + idade + " anos");
+        System.out.println ("Meu CPF é" + cpf);
+
+        sc.close();
+
+
+    }
+}
