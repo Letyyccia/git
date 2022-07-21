5b5704e6054b44b34618ef6443a3fd6a8e534581
Letyyccia <102097499+Letyyccia@users.noreply.github.com>
2022-07-21 16:22:23 -0300

    Create autor.java

---
+++ autor.java
@@ -0,0 +1,13 @@
+public class autor {
+    String nome;
+    int idade;
+    String naturalidade;
+    String cpf;
+    
+    public autor(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    
+   
+}
